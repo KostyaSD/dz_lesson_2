5156bdb777978a493a746b496726daa8dfde9d2f
RazerPro <Drakula3D@mail.ru>
2021-11-11 00:20:59 +0300

    example-commit

--- src/main/java/com/geekbrains/Main.java
+++ src/main/java/com/geekbrains/Main.java
@@ -5,13 +5,16 @@ public class Main {
         System.out.println(number(2,3));
         System.out.println(positiveAndNegative(0));
         System.out.println(booleanPositiveAndNegative(-2));
-    }
+        stringAndNumber("hello world", 2);
+        System.out.println(leapYear(400));
+     }
 
 /* 1. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит
 в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.*/
 
     public static boolean number(int a, int b) {
-        if ((a + b) >= 10 && (a + b) <= 20) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
             return true;
         } else return false;
     }
@@ -37,4 +40,23 @@ public class Main {
 
 /* 4. Написать метод, которому в качестве аргументов передается строка и число,
 метод должен отпечатать в консоль указанную строку, указанное количество раз.*/
+
+    public static void stringAndNumber(String text, int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.println(text);
+        }
+    }
+
+/* 5. * Написать метод, который определяет, является ли год високосным,
+и возвращает boolean (високосный - true, не високосный - false).
+Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+ */
+
+    public static boolean leapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        } else return false;
+    }
 }
